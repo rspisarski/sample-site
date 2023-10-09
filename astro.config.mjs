6a26316b55ef96e92bb9894eff06eade852f85db
rspisarski <rspisarski@gmail.com>
2023-10-09 17:38:56 -0400

    StoryBlok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,30 @@
 import { defineConfig } from 'astro/config';
 
+import storyblok from '@storyblok/astro';
 import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: 
+  [
+    tailwind(),
+    storyblok({
+      accessToken: 'nzUR3BcrSZARXfzR61oh0wtt',
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        splitPanel: 'storyblok/SplitPanel',
+      },
+      apiOptions: {
+        region: 'us',
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
